20123c06a813d6d1cc06ecb43f2c969453692445
dev-augustin <dev.augustin00@gmail.com>
2020-05-15 15:16:00 -0500

    trying to make fetch request with REACT APP Key

--- poster-app/src/components/Home.js
+++ poster-app/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const API_KEY=process.env.REACT_APP_API_KEY;
+console.log(process.env.REACT_APP_API_KEY)
+
 export default class Home extends Component {
     constructor(props){
         super(props);
@@ -14,10 +17,11 @@ export default class Home extends Component {
         this.searchRequest();
     }
     searchRequest = async () =>{
+        console.log(API_KEY)
         try{
             const response = await axios.get("https://api.pexels.com/v1/search?query=people",
             {headers: {
-                "Authorization" : ""
+                "Authorization" : "API_KEY"
               }
             })
 
