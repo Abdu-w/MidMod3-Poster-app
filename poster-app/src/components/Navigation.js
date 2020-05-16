4aa61df3e82676304400e44aed9309161153cb4a
Student_GA113 <Student_GA113@Student-GA113.local>
2020-05-16 14:44:45 -0400

    add css files

--- poster-app/src/components/Navigation.js
+++ poster-app/src/components/Navigation.js
@@ -3,25 +3,27 @@ import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home';
 import Upload from './Upload';
 import About from './About';
+import '../styles/Navigation.css'
 
 function Navigation() {
 	return (
 		<Router>
 			<nav className="container">
+			<div className= "logo">Poster Maker</div>
 				<ul className="navbar">
 					<li>
-						<Link to="/">Home</Link>
+						<Link to = "/home">Home</Link>
 					</li>
 					<li>
-						<Link to="/rights">Upload</Link>
+						<Link to= "/upload">Upload</Link>
 					</li>
 					<li>
-						<Link to="/stakeholders">About</Link>
+						<Link to = "/about">About</Link>
 					</li>
 				</ul>
 			</nav>
 			<Switch>
-				<Route exact path="/" component={Home}></Route>
+				<Route exact path="/home" component={Home}></Route>
 				<Route exact path="/upload" component={Upload}></Route>
 				<Route exact path="/about" component={About}></Route>
 			</Switch>
