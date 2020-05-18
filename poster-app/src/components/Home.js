c410508c5639c795560ddd0ffd23bbce29a3f757
dev-augustin <dev.augustin00@gmail.com>
2020-05-17 21:18:04 -0500

    moved Home search content to new Search Route

--- poster-app/src/components/Home.js
+++ poster-app/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import '../styles/Home.css';
+import ErrorMessage from './ErrorMessage';
 import {Form, FormControl, Button} from "react-bootstrap";
 
 const API_KEY=process.env.REACT_APP_API_KEY;
@@ -13,26 +14,15 @@ export default class Home extends Component {
             displayResults: '',
             displayData:[ ],
             img: '',
-            searchText:''
+            searchText:'',
+            resultLength: ''
         }
     }
  
-    // componentDidMount(){
-    //     this.searchRequest();
-    // }
-    
-     searchText = (e) =>{
-            e.preventDefault();
-            console.log(e.target.value);
-            this.setState({searchText : e.target.value})
-     }
-
-     handleSearch = (e) =>{
-        e.preventDefault();
-        console.log(this.state.searchText);
+    componentDidMount(){
         this.searchRequest();
- }
-
+    }
+    
     searchRequest = async () =>{
         console.log(API_KEY)
         try{
@@ -43,7 +33,7 @@ export default class Home extends Component {
             // })
             console.log(this.state.searchText)
             const searchText = this.state.searchText;
-            const response = await axios.get("https://api.pexels.com/v1/search?query=" +searchText ,
+            const response = await axios.get("https://api.pexels.com/v1/search?query=art" ,
             {headers: {
                 "Authorization" : API_KEY
               }
@@ -56,34 +46,38 @@ export default class Home extends Component {
             // let url=response.data.photos[0].src.portrait;
             //console.log(url)
         // this.setState({img : url});
+        let resultLength = response.data.total_results;
         let url=response.data.photos;
-        this.setState({displayData : url});
+        
+            this.setState({displayData : url});
             
-        }
+       }
         catch(e){
             console.log(e);
         }
     }
     render() {
-        console.log(this.state.displayData )
+        console.log(this.state.displayData );
+        //let response = this.state.displayData;
         return (
-            <div>
-                <Form className="search-container">
+            <React.Fragment>
+                {/* <Form className="search-container">
                     <FormControl type="text" value={this.state.searchText} id="search-text" onChange={this.searchText} placeholder="search text" />
                     <Button  id="search-button" onClick={this.handleSearch}>Search</Button>
-                    {/* <FormText id="form-text">Search for your favorite category</FormText>
-                    <Form.T */}
-                </Form>
-                <div className="results-container"> {this.state.displayData.map((result) => <div className="poster-results"><ul key= {result.id} className="lists-display">
-                    <li className="results-li"> <img src= {result.src.portrait} alt="different Images" height = "500px" width="300px" /> <h5>Poster ID: {result.id} </h5> <Button id="view-details" variant="link">View Details</Button>  </li>
-                   
-                </ul>
-
-                </div>)} </div>
+                </Form> */}
+                <div className="results-container"> 
+                {this.state.displayData.map((result) => {
+                    return(
+                        <div className="poster-results"><ul key= {result.id} className="lists-display">
+                        <li className="results-li"> <img src= {result.src.portrait} alt="different Images" height = "400px" width="260px" /></li>
+                        </ul> </div>)
+                    } )
+                }  
+                </div>
                {/* {this.searchRequest()} */}
                {/* <img src = "https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800" /> */}
               
-            </div>
+               </React.Fragment>
         )
     }
 }
